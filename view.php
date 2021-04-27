f0f14a6b75b8d9bc7622d95976ffff5e08ef43d6
PulsarFox <pascal@plumia.net>
2021-04-27 19:50:51 +0200

    Add title text

--- view.php
+++ view.php
@@ -5,7 +5,7 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Mon IP - Plumia</title>
+        <title>Mon IP - Plumia MyIP</title>
         <link rel="shortcut icon" type="image/jpg" href="https://plumia.net/images/favicon.png"/>
         <link rel="stylesheet" href="css/output.css" />
     </head>
